79cbb6a6f1ecc31d3297a2e5d8682a489f6dec3c
Basile Lecouturier <50240788+bolex222@users.noreply.github.com>
2025-05-22 18:54:28 +0200

    feat: add path parser working

--- cmd/svg-cli/main.go
+++ cmd/svg-cli/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bolex222/svg-cli/internal/parser"
+)
 
 func main() {
 
-	fmt.Printf("here start the project")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("The path are required")
+		os.Exit(1)
+	}
+
+	allMotionsAsString := args[1]
+	motions, err := parser.ParseMotions(allMotionsAsString)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, val := range motions {
+		fmt.Printf("motion: %c, values: %v\n", val.Letter, val.Values)
+	}
 
 }
---
+++ internal/parser/parser.go
@@ -0,0 +1,119 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"unicode"
+)
+
+var VaslidPathChar = []rune{
+	rune('M'), rune('m'), rune('L'), rune('l'),
+	rune('H'), rune('h'), rune('V'), rune('v'),
+	rune('C'), rune('c'), rune('S'), rune('s'),
+	rune('Q'), rune('q'), rune('T'), rune('t'),
+	rune('A'), rune('a'), rune('Z'), rune('z'),
+}
+
+type pathMotion struct {
+	Letter rune
+	Values [7]float64
+}
+
+type pathMotions []pathMotion
+
+func createNextMotion(char rune, motions *pathMotions) error {
+	err := CheckCharIsValidMotionb(char)
+	if err != nil {
+		return err
+	}
+	*motions = append(*motions, pathMotion{
+		Letter: char,
+	})
+	return nil
+}
+
+func incrementCurrentPathPoint(char rune, motions *pathMotions, currentMotionIndex, currentValuesIndex, charIncrement, decimalIndex int) error {
+
+	if currentMotionIndex < 0 {
+		return errors.New("a letter is expected to begin a path")
+	}
+	if currentValuesIndex >= 7 {
+		return fmt.Errorf("too many values for motion %c provided at pos %v", (*motions)[currentMotionIndex].Letter, charIncrement)
+	}
+	if decimalIndex > 0 {
+		factor := 1 / math.Pow10(decimalIndex)
+		parsedChar, err := strconv.ParseFloat(string(char), 64)
+		if err != nil {
+			return fmt.Errorf("character %c at position %v could not be converted to a float2", char, charIncrement)
+		}
+		(*motions)[currentMotionIndex].Values[currentValuesIndex] += parsedChar * factor
+	} else {
+		newVal, err := strconv.ParseFloat(string(char), 64)
+		if err != nil {
+			return fmt.Errorf("character %c at position %v could not be converted to a float2", char, charIncrement)
+		}
+
+		(*motions)[currentMotionIndex].Values[currentValuesIndex] *= 10
+		(*motions)[currentMotionIndex].Values[currentValuesIndex] += newVal
+	}
+	return nil
+}
+
+func ParseMotions(fullPath string) ([]pathMotion, error) {
+	var motions pathMotions
+	charIncrement := 0
+	currentMotionIndex := -1
+	decimalIndex := 0
+	currentValuesIndex := 0
+	currentDigitIndex := 0
+
+	for pos, char := range fullPath {
+		switch {
+		case unicode.IsLetter(char):
+			err := createNextMotion(char, &motions)
+			if err != nil {
+				return nil, err
+			}
+
+			currentMotionIndex++
+			currentValuesIndex = 0
+			currentDigitIndex = 0
+			decimalIndex = 0
+
+		case unicode.IsDigit(char):
+			currentDigitIndex++
+			err := incrementCurrentPathPoint(char, &motions, currentMotionIndex, currentValuesIndex, charIncrement, decimalIndex)
+			if err != nil {
+				return nil, err
+			}
+			if decimalIndex > 0 {
+				decimalIndex++
+			}
+
+		case char == 46: // char is period
+			fmt.Printf("char: %c \n", char)
+			decimalIndex++
+
+		case char == 44 || char == 32: // char is space or ,
+			fmt.Printf("char: %c \n", char)
+			if currentDigitIndex > 0 {
+				currentValuesIndex++
+				decimalIndex = 0
+			}
+
+		default:
+			return nil, fmt.Errorf("invalid character %c at position %v", char, pos)
+		}
+	}
+	return motions, nil
+}
+
+func CheckCharIsValidMotionb(char rune) error {
+	if !slices.Contains(VaslidPathChar, char) {
+		return errors.New(string(char) + " is not a valid path character.")
+	}
+	return nil
+}
