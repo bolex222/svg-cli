be5f8fd2546c465917c49abbd8f2b0b13cb2e699
Basile Lecouturier <50240788+bolex222@users.noreply.github.com>
2025-07-11 00:23:24 +0200

    refactor(tokenizer): cleaner way to handle current Token

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -22,55 +22,73 @@ type Token struct {
 }
 
 type Tokenizer struct {
-	r          *bufio.Reader
-	tokens     []Token
-	tokenIndex int
+	reader            *bufio.Reader
+	currentToken *Token
+	tokens       []Token
 }
 
-func New() Tokenizer {
-	return Tokenizer{
-		tokens:     make([]Token, 0),
-		tokenIndex: -1,
+func New() *Tokenizer {
+	return &Tokenizer{
+		tokens:       make([]Token, 0),
+		currentToken: nil,
 	}
 }
 
 func (t *Tokenizer) nextToken(tokenType TokenType, initialString string) {
-	t.tokenIndex++
-	t.tokens = append(t.tokens, Token{
+	t.currentToken = &Token{
 		Type:  tokenType,
 		Value: initialString,
-	})
+	}
+}
+
+func (t *Tokenizer) finishCurrentToken() {
+	if t.currentToken != nil {
+		t.tokens = append(t.tokens, *t.currentToken)
+		t.currentToken = nil
+	}
 }
 
 func (t *Tokenizer) isCurrentlyNumberToken() bool {
-	return t.tokenIndex >= len(t.tokens)-1 && t.tokens[t.tokenIndex].Type == TokenNumber
+	return t.currentToken != nil && t.currentToken.Type == TokenNumber
 }
 
-func (t *Tokenizer) Tokenize(r *bufio.Reader) ([]Token, error) {
-	t.r = r
+func (t *Tokenizer) appendToCurrentToken(char rune) error {
+	if t.currentToken == nil {
+		return fmt.Errorf("no token to append to")
+	}
+	t.currentToken.Value += string(char)
+	return nil
+}
+
+func (t *Tokenizer) Tokenize(reader *bufio.Reader) ([]Token, error) {
+	t.reader = reader
 	i := 0
 
 	for {
-		b, err := r.ReadByte()
+		b, err := reader.ReadByte()
 		if err != nil {
+			t.finishCurrentToken()
 			return t.tokens, nil
 		}
 		char := rune(b)
 
 		switch {
+
 		case char == 'e':
 			if !t.isCurrentlyNumberToken() {
 				return t.tokens, fmt.Errorf("number token can not start by \"e\" at position %v", i)
 			}
 
-			if strings.Contains(t.tokens[t.tokenIndex].Value, "e") {
+			if strings.Contains(t.currentToken.Value, "e") {
 				return t.tokens, fmt.Errorf("token already contain a \"e\" at positoin %v", i)
 			}
 
-			t.tokens[t.tokenIndex].Value += string(char)
+			if err := t.appendToCurrentToken(char); err != nil {
+				return t.tokens, err
+			}
 
 		case char == '-':
-			p, err := r.Peek(1)
+			p, err := reader.Peek(1)
 			if err != nil {
 				return t.tokens, fmt.Errorf("character \"-\" can not be a last char")
 			}
@@ -79,39 +97,35 @@ func (t *Tokenizer) Tokenize(r *bufio.Reader) ([]Token, error) {
 				return t.tokens, fmt.Errorf("character \"-\" can not be isolated")
 			}
 
+			t.finishCurrentToken()
 			t.nextToken(TokenNumber, string(char))
 
 		case char == '.':
-			if !t.isCurrentlyNumberToken() || strings.Contains(t.tokens[t.tokenIndex].Value, ".") {
+			if !t.isCurrentlyNumberToken() || strings.Contains(t.currentToken.Value, ".") {
+				t.finishCurrentToken()
 				t.nextToken(TokenNumber, string(char))
-			} else {
-				t.tokens[t.tokenIndex].Value += "."
+			} else if err := t.appendToCurrentToken('.'); err != nil {
+				return t.tokens, err
 			}
 
 		case unicode.IsNumber(char):
-			if !t.isCurrentlyNumberToken(){
+			if !t.isCurrentlyNumberToken() {
+				t.finishCurrentToken()
 				t.nextToken(TokenNumber, string(char))
-			} else {
-				t.tokens[t.tokenIndex].Value += string(char)
+			} else if err := t.appendToCurrentToken(char); err != nil {
+				return t.tokens, err
 			}
 
 		case unicode.IsLetter(char):
 			// TODO: check if command is a valid letter
+			t.finishCurrentToken()
 			t.nextToken(TokenCommand, string(char))
 
 		case char == ' ' || char == ',':
-			p, err := r.Peek(1)
-			if err != nil {
-				return t.tokens, nil
-			}
-			pChar := rune(p[0])
-
-			if unicode.IsNumber(pChar) {
-				t.nextToken(TokenNumber, "")
-			}
+			t.finishCurrentToken()
 
 		default:
-			return t.tokens, fmt.Errorf("unexpected character \"%c\" at pos %v", char, i +1)
+			return t.tokens, fmt.Errorf("unexpected character \"%c\" at pos %v", char, i+1)
 		}
 		i++
 	}
